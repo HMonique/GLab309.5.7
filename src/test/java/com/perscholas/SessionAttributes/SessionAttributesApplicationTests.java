8f287223b3b9e312437d4a5a34fad79860dc2b97
HMonique <moniquehl@outlook.com>
2024-09-12 10:26:35 -0400

    first commit

---
+++ src/test/java/com/perscholas/SessionAttributes/SessionAttributesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.SessionAttributes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionAttributesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
